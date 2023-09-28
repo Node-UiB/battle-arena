02168b42e6d33869e674ee19b8f1c0539b24510f
KrHaf <03krhaf@gmail.com>
2023-09-28 16:31:18 +0200

    Lagt inn move og rot i Agenttemplet

--- classes/AgentTemplate.py
+++ classes/AgentTemplate.py
@@ -6,7 +6,7 @@ from .Projectile import Projectile
 
 
 class AgentTemplate(pygame.sprite.Sprite):
-    def __init__(self, pos, all_sprites, bullets) -> None:
+    def __init__(self, pos, rot, all_sprites, bullets) -> None:
         super().__init__()
         self.image = pygame.Surface([20, 20])
         self.image.fill(con.BLACK)
@@ -14,6 +14,12 @@ class AgentTemplate(pygame.sprite.Sprite):
         self.all_sprites = all_sprites
         self.add(self.all_sprites)
         self.bullets = bullets
+
+        self.pos = pos
+        self.speed = 0.1
+        self.rot = rot
+        self.rotspeed = 0.1
+        self.size = 35
 
     def update(self):
         """Update position of player"""
@@ -29,6 +35,27 @@ class AgentTemplate(pygame.sprite.Sprite):
             Projectile(
                 pygame.mouse.get_pos(), angle, 5, 0.1, self.all_sprites, self.bullets
             )
+    def move(self, dir = 0):
+        # passer på at dir ikke er mer enn 1 eller mindre enn -1
+        dir = min(1, max(-1, dir))
+        
+        """ hvis vi trenger dette
+        # beveger seg i retningen du ser og ikke går ut av rammen
+
+        if self.size/2 <= self.pos[0] + sin(radians(self.rot))*self.speed*dir <= width-self.size/2:
+            self.pos[0] += sin(radians(self.rot))*self.speed*dir
+        if self.size/2 <= self.pos[1] + cos(radians(self.rot))*self.speed*dir <= height-self.size/2:
+            self.pos[1] += cos(radians(self.rot))*self.speed*dir
+        """
+        # beveger seg i retningen du ser
+        self.pos[0] += math.sin(math.radians(self.rot))*self.speed*dir
+        self.pos[1] += math.cos(math.radians(self.rot))*self.speed*dir
+    
+    def rotate(self, dir = 1):
+        # passer på at dir ikke er mer enn 1 eller mindre enn -1
+        dir = min(1, max(-1, dir))
+
+        self.rot = (self.rot+dir*self.rotspeed)%360
 
     def shoot(self):
         print("pew")
