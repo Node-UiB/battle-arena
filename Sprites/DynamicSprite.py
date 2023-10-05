182d5b16c09ef9817894b1eea053063668f6f448
simsam8 <simvedaa@gmail.com>
2023-10-05 14:59:29 +0200

    class template

--- Sprites/DynamicSprite.py
+++ Sprites/DynamicSprite.py
@@ -8,3 +8,13 @@ Inherits from:
 update : dt -> None
     Updates internal transfrom
 """
+
+from Sprites.PolygonSprite import PolygonSprite
+
+
+class DynamicSprite(PolygonSprite):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def update(self, dt) -> None:
+        pass
